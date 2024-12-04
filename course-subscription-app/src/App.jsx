09c05cc875763b7dfe3e96641a971a295d487c2e
Josephine Closan <89244648+JoeClos@users.noreply.github.com>
2024-12-04 17:01:02 +0200

    change admin's navbar

--- course-subscription-app/src/App.jsx
+++ course-subscription-app/src/App.jsx
@@ -1,5 +1,114 @@
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import CourseList from "./pages/CourseList";
+// import Navbar from "./components/Navbar";
+// import MyCourses from "./pages/MyCourses";
+// import Login from "./pages/Login";
+// import ProtectedRoute from "./components/ProtectedRoute";
+// import Dashboard from "./pages/admin/Dashboard";
+// import ManageUsers from "./pages/admin/ManageUsers";
+// import ManageSubscriptions from "./pages/admin/ManageSubscriptions";
+// import ManageCourses from "./pages/admin/ManageCourses";
+// import Analytics from "./pages/admin/Analytics";
+// import Register from "./pages/Register";
+// import { useCart } from "./store/CartContext";
+// import { fetchSubscribedCourses, unsubscribeFromCourse } from "./utils/api";
+
+// function App() {
+//   const { updateSubscribedCourses } = useCart();
+//   const [searchQuery, setSearchQuery] = useState("");
+  
+
+//   const handleSearch = (query) => {
+//     setSearchQuery(query);
+//   };
+
+//   useEffect(() => {
+//     const learnerId = localStorage.getItem("learnerId");
+//     if (!learnerId) return;
+
+//     const loadSubscribedCourses = async () => {
+//       try {
+//         const courses = await fetchSubscribedCourses(learnerId);
+//         updateSubscribedCourses(courses);
+//       } catch (error) {
+//         console.error("Failed to load subscribed courses:", error);
+//       }
+//     };
+
+//     loadSubscribedCourses();
+//   }, [updateSubscribedCourses]);
+
+//   return (
+//     <div className="main">
+//       <Router>
+//         <Navbar onSearch={handleSearch} />
+//         <div className="content">
+//           <Routes>
+//             <Route path="/register" element={<Register />} />
+//             <Route path="/login" element={<Login />} />
+//             <Route
+//               path="/dashboard"
+//               element={
+//                 <ProtectedRoute requiredRole="admin">
+//                   <Dashboard />
+//                 </ProtectedRoute>
+//               }
+//             />
+//             <Route
+//               path="/dashboard/users"
+//               element={
+//                 <ProtectedRoute requiredRole="admin">
+//                   <ManageUsers />
+//                 </ProtectedRoute>
+//               }
+//             />
+//             <Route
+//               path="/dashboard/subscriptions"
+//               element={
+//                 <ProtectedRoute requiredRole="admin">
+//                   <ManageSubscriptions />
+//                 </ProtectedRoute>
+//               }
+//             />{" "}
+//             <Route
+//               path="/dashboard/courses"
+//               element={
+//                 <ProtectedRoute requiredRole="admin">
+//                   <ManageCourses />
+//                 </ProtectedRoute>
+//               }
+//             />{" "}
+//             <Route
+//               path="/dashboard/analytics"
+//               element={
+//                 <ProtectedRoute requiredRole="admin">
+//                   <Analytics />
+//                 </ProtectedRoute>
+//               }
+//             />
+//             <Route
+//               path="/"
+//               element={<CourseList searchQuery={searchQuery} />}
+//             />
+//             <Route
+//               path="/my-courses"
+//               element={
+//                 <MyCourses unsubscribeFromCourse={unsubscribeFromCourse} />
+//               }
+//             />
+//           </Routes>
+//         </div>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import CourseList from "./pages/CourseList";
 import Navbar from "./components/Navbar";
 import MyCourses from "./pages/MyCourses";
@@ -13,9 +122,11 @@ import Analytics from "./pages/admin/Analytics";
 import Register from "./pages/Register";
 import { useCart } from "./store/CartContext";
 import { fetchSubscribedCourses, unsubscribeFromCourse } from "./utils/api";
+import AuthContext from "./store/AuthContext";
 
 function App() {
   const { updateSubscribedCourses } = useCart();
+  const { user } = useContext(AuthContext)
   const [searchQuery, setSearchQuery] = useState("");
 
   const handleSearch = (query) => {
@@ -38,11 +149,15 @@ function App() {
     loadSubscribedCourses();
   }, [updateSubscribedCourses]);
 
+  const isAdmin = user?.role === 'admin';
+  // console.log("🚀 ~ App ~ isAdmin:", isAdmin)
+
   return (
     <div className="main">
       <Router>
-        <Navbar onSearch={handleSearch} />
-        <div className="content">
+        {!isAdmin && <Navbar onSearch={handleSearch} />} 
+        {/* <Navbar onSearch={handleSearch} /> */}
+        <div className="content" style={{ paddingTop: !isAdmin ? "60px" : "0" }}> {/* Adjust padding based on Navbar */}
           <Routes>
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
@@ -53,48 +168,44 @@ function App() {
                   <Dashboard />
                 </ProtectedRoute>
               }
-            />
-            <Route
-              path="/dashboard/users"
-              element={
-                <ProtectedRoute requiredRole="admin">
-                  <ManageUsers />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/dashboard/subscriptions"
-              element={
-                <ProtectedRoute requiredRole="admin">
-                  <ManageSubscriptions />
-                </ProtectedRoute>
-              }
-            />{" "}
-            <Route
-              path="/dashboard/courses"
-              element={
-                <ProtectedRoute requiredRole="admin">
-                  <ManageCourses />
-                </ProtectedRoute>
-              }
-            />{" "}
-            <Route
-              path="/dashboard/analytics"
-              element={
-                <ProtectedRoute requiredRole="admin">
-                  <Analytics />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/"
-              element={<CourseList searchQuery={searchQuery} />}
-            />
+            >
+              <Route
+                path="users"
+                element={
+                  <ProtectedRoute requiredRole="admin">
+                    <ManageUsers />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="subscriptions"
+                element={
+                  <ProtectedRoute requiredRole="admin">
+                    <ManageSubscriptions />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="courses"
+                element={
+                  <ProtectedRoute requiredRole="admin">
+                    <ManageCourses />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="analytics"
+                element={
+                  <ProtectedRoute requiredRole="admin">
+                    <Analytics />
+                  </ProtectedRoute>
+                }
+              />
+            </Route>
+            <Route path="/" element={<CourseList searchQuery={searchQuery} />} />
             <Route
               path="/my-courses"
-              element={
-                <MyCourses unsubscribeFromCourse={unsubscribeFromCourse} />
-              }
+              element={<MyCourses unsubscribeFromCourse={unsubscribeFromCourse} />}
             />
           </Routes>
         </div>
--- course-subscription-app/src/pages/admin/Dashboard.jsx
+++ course-subscription-app/src/pages/admin/Dashboard.jsx
@@ -6,47 +6,209 @@
 
 // export default Dashboard;
 
+// import {
+//   Box,
+//   Toolbar,
+//   List,
+//   ListItem,
+//   ListItemText,
+//   Typography,
+// } from "@mui/material";
+// import { Link, Outlet } from "react-router-dom";
+
+// const DashboardLayout = () => {
+//   return (
+//     <Box>
+//       <Typography variant="h4" noWrap>
+//         Admin Dashboard
+//       </Typography>
+//       <Box sx={{ display: "flex" }}>
+
+//         <Box component="nav" sx={{ width: { sm: 240 }, flexShrink: { sm: 0 } }}>
+//           <List>
+//             <ListItem button component={Link} to="users">
+//               <ListItemText primary="Manage Users" />
+//             </ListItem>
+//             <ListItem button component={Link} to="subscriptions">
+//               <ListItemText primary="Manage Subscriptions" />
+//             </ListItem>
+//             <ListItem button component={Link} to="courses">
+//               <ListItemText primary="Manage Courses" />
+//             </ListItem>
+//             <ListItem button component={Link} to="analytics">
+//               <ListItemText primary="Analytics" />
+//             </ListItem>
+//           </List>
+//         </Box>
+//         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+//           <Toolbar />
+//           <Outlet />
+//         </Box>
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default DashboardLayout;
+
+import { useContext, useState } from "react";
+import PropTypes from "prop-types";
 import {
+  AppBar,
   Box,
-  Toolbar,
+  CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
   List,
   ListItem,
   ListItemText,
+  ListItemButton,
+  Toolbar,
   Typography,
 } from "@mui/material";
+import { Menu as MenuIcon } from "@mui/icons-material";
 import { Link, Outlet } from "react-router-dom";
+import { ExitToApp } from "@mui/icons-material";
+import AuthContext from "../../store/AuthContext";
 
-const DashboardLayout = () => {
+const drawerWidth = 240;
+
+const Dashboard = (props) => {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const { logout } = useContext(AuthContext);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const handleLogout = () => {
+    logout();
+  };
+
+  const drawer = (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        <ListItem button="true" component={Link} to="users">
+          <ListItemText primary="Manage Users" />
+        </ListItem>
+        <ListItem button="true" component={Link} to="subscriptions">
+          <ListItemText primary="Manage Subscriptions" />
+        </ListItem>
+        <ListItem button="true" component={Link} to="courses">
+          <ListItemText primary="Manage Courses" />
+        </ListItem>
+        <ListItem button="true" component={Link} to="analytics">
+          <ListItemText primary="Analytics" />
+        </ListItem>
+      </List>
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
   return (
-    <Box>
-      <Typography variant="h4" noWrap>
-        Admin Dashboard
-      </Typography>
-      <Box sx={{ display: "flex" }}>
+    <Box sx={{ display: "flex"}}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, padding: "11px", backgroundColor: "#201F40"}}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 4, display: { sx: "none" } }}
+          >
+            Admin Dashboard
+          </Typography>
 
-        <Box component="nav" sx={{ width: { sm: 240 }, flexShrink: { sm: 0 } }}>
-          <List>
-            <ListItem button component={Link} to="users">
-              <ListItemText primary="Manage Users" />
-            </ListItem>
-            <ListItem button component={Link} to="subscriptions">
-              <ListItemText primary="Manage Subscriptions" />
-            </ListItem>
-            <ListItem button component={Link} to="courses">
-              <ListItemText primary="Manage Courses" />
-            </ListItem>
-            <ListItem button component={Link} to="analytics">
-              <ListItemText primary="Analytics" />
-            </ListItem>
-          </List>
-        </Box>
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <Toolbar />
-          <Outlet />
-        </Box>
+          <ListItemButton
+            onClick={handleLogout}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-start",
+              gap: 1,
+            }}
+          >
+            <Typography variant="h6" noWrap component="div">
+              Logout
+            </Typography>
+            {/* <ListItemText
+              primary="Logout"
+              variant="h6" noWrap component="div" 
+            /> */}
+            <ExitToApp fontSize="small" />
+          </ListItemButton>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar />
+        <Outlet />
       </Box>
     </Box>
   );
 };
 
-export default DashboardLayout;
+Dashboard.propTypes = { window: PropTypes.func };
+
+export default Dashboard;
