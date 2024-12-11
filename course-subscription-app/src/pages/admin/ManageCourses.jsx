64c9e41c366776188794b80f72b099b87f4c012e
Josephine Closan <89244648+JoeClos@users.noreply.github.com>
2024-12-11 14:24:17 +0200

    fetch courses - admin role

--- course-subscription-app/src/pages/admin/ManageCourses.jsx
+++ course-subscription-app/src/pages/admin/ManageCourses.jsx
@@ -1,7 +1,106 @@
+import { useState, useEffect } from "react";
+import { fetchCourses } from "../../utils/api";
+import {
+  Typography,
+  Box,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Paper,
+} from "@mui/material";
+
 const ManageCourses = () => {
+  const [courses, setCourses] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getCourses = async () => {
+      try {
+        const response = await fetchCourses();
+        setCourses(response);
+        console.log("🚀 ~ getCourses ~ response:", response);
+      } catch (error) {
+        setError("Error fetching users", error);
+      }
+    };
+    getCourses();
+  }, []);
+
+  if (error) {
     return (
-        <div><h1>Courses</h1></div>
-    )
-}
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+      >
+        <Typography variant="h6" color="error">
+          {error}
+        </Typography>
+      </Box>
+    );
+  }
+
+  return (
+    <Box>
+      <Typography variant="h4" gutterBottom mt={8}>
+        Total Courses: {courses.length}
+      </Typography>{" "}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="user table">
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ fontWeight: "bold", fontSize: "16px" }}>
+                Ttile
+              </TableCell>
+              <TableCell sx={{ fontWeight: "bold", fontSize: "16px" }}>
+                Type
+              </TableCell>
+              <TableCell sx={{ fontWeight: "bold", fontSize: "16px" }}>
+                Description
+              </TableCell>
+              <TableCell sx={{ fontWeight: "bold", fontSize: "16px" }}>
+                Duration
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {courses.map((course) => (
+              <TableRow key={course._id}>
+                <TableCell>{course.title}</TableCell>
+                <TableCell>{course.courseType}</TableCell>
+                <TableCell>{course.description}</TableCell>
+                <TableCell>
+                  {course.duration
+                    ? `${
+                        course.duration.days && course.duration.days
+                          ? " and "
+                          : ""
+                      }${
+                        course.duration.hours
+                          ? `${course.duration.hours} hours`
+                          : ""
+                      }${
+                        course.duration.hours && course.duration.minutes
+                          ? " and "
+                          : ""
+                      }${
+                        course.duration.minutes
+                          ? `${course.duration.minutes} minutes`
+                          : ""
+                      }`
+                    : "No duration available"}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
 
 export default ManageCourses;
--- course-subscription-app/src/utils/api.js
+++ course-subscription-app/src/utils/api.js
@@ -114,10 +114,9 @@ export const fetchUsers = async () => {
   try {
     const response = await axios.get(endpoint);
     return response;
-    console.log("🚀 ~ fetchUsers ~ response.data:", response)
-
   } catch (error) {
     console.error("Error fetching users:", error);
     throw error;
   }
 };
+
